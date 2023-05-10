863223241b198577cc044ee3ae745a11abc59f6e
Luitorres <luigi.torres073@gmail.com>
2023-05-10 23:21:00 +0800

    try integ

--- config.php
+++ config.php
@@ -1,5 +1,18 @@
 <?php
-
-$conn = mysqli_connect('localhost','root','','sbit3n_db') or die('connection failed');
-
-?>
+$servername = "sql596.main-hosting.eu";
+$database = "u876447700_integration";
+$username = "u876447700_root";
+$password = "Admin123";
+ 
+// Create connection
+ 
+$conn = mysqli_connect($servername, $username, $password, $database);
+ 
+// Check connection
+ 
+if (!$conn) {
+ 
+    die("Connection failed: " . mysqli_connect_error());
+ 
+}
+// echo "Connected successfully";
